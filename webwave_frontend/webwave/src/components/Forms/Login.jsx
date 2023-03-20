57a13bbf4784b85e79bec75f05a42a688439c88b
Nwachukwu Justin <joelnwachukwu67@gmail.com>
2023-03-20 13:24:34 +0100

    Update Login.jsx

--- webwave_frontend/webwave/src/components/Forms/Login.jsx
+++ webwave_frontend/webwave/src/components/Forms/Login.jsx
@@ -1,20 +1,25 @@
 const LoginForm = () => {
     return ( 
-        <div className="w-[300px] flex flex-col gap-y-8 bg-white text-black px-8 py-10">
+        <div className="w-[300px] flex flex-col py-10">
 
-            <h1 className="text-3xl">LOGIN</h1>
+            <h1 className="text-3xl font-bold mb-4">Log in</h1>
+            <h2 className="mb-8">Hey, welcome back👋</h2>
 
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="text" id="email" className="bg-transparent w-full focus:border-t-0 border-b border-b-gray-500 outline-none px-3 py-2 focus:border-b-mainColor duration-150 text-sm"/>
+            <div className="bg-[#181818] rounded py-2 px-2 mb-4">
+                
+                <input type="text" id="email" className="bg-transparent text-gray-300 w-full outline-none px-3 py-2 duration-150 text-sm" placeholder="Email"/>
             </div>
 
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" className="bg-transparent w-full focus:border-t-0 border-b border-b-gray-500 outline-none px-3 py-2 focus:border-b-mainColor duration-150 text-sm"/>
+            <div className="bg-[#181818] rounded py-2 px-2 mb-1">
+                <input type="password" id="password" className="bg-transparent text-gray-300 w-full outline-none px-3 py-2 duration-150 text-sm" placeholder="Password"/>
             </div>
 
-            <button className="bg-black py-4 text-white">Login</button>
+
+            <p className="text-right text-mainColor mb-4 hover:text-mainHover duration-200 cursor-pointer">Forgot Password?</p>
+
+            <button className="bg-mainColor hover:bg-mainHover py-4 text-white mt-4 duration-200 rounded">Login</button>
+
+            
         </div>
      );
 }
